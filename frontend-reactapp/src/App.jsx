1d54dd2ca7a8a6e23326c32bd86a13af2cdae96e
rohithchowdary2005 <rohithkodavati999@gmail.com>
2025-10-18 11:41:24 +0530

    Update App.jsx

--- frontend-reactapp/src/App.jsx
+++ frontend-reactapp/src/App.jsx
@@ -1,21 +1,14 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import Navbar from "./components/Navar";
+import Navbar from "./components/NavBar";
 
 function App() {
   return (
     <BrowserRouter>
-<<<<<<< HEAD
       <Navbar/>
-=======
-      <Navbar />
->>>>>>> 24ff6eff39f8aad9d3d0cefbc42c40cf0b1adeb6
     </BrowserRouter>
   );
 }
 
 export default App;
-<<<<<<< HEAD
-=======
- 
->>>>>>> 24ff6eff39f8aad9d3d0cefbc42c40cf0b1adeb6
+
